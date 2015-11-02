95c07da196b8029099c0acea671e03e80f1f293f
javi <jaanga@live.com>
2015-11-02 23:44:41 +0100

    se crean los achivos de la app web

---
+++ vista.html.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>App Web cuadros</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/css/bootstrap.css">
+</head>
+<body>
+	<h1>Cuadros</h1>
+</body>
+</html>
